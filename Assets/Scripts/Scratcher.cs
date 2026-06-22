d3ab02a5c604e177af4fd3d2140b374f1493d188
Joseph Isaacs <joseph.isaacs@mymail.champlain.edu>
2026-06-22 16:46:37 -0400

    Pretty good, still sounds silly

--- Assets/Scripts/Scratcher.cs
+++ Assets/Scripts/Scratcher.cs
@@ -3,105 +3,104 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using System.Runtime.CompilerServices;
-using System;
 
 public class Scratcher : MonoBehaviour
 {
     [SerializeField] AudioSource source;
     [SerializeField] GameObject record;
+    [SerializeField] float followLag = 0.08f;
+    [SerializeField] float catchUpSeconds = 0.1f;
+    [SerializeField] float maxPitch = 10f;
+
+    float targetTimeProgress = 0f;
+    float timeProgress => source.time / source.clip.length;
+
+    float audioProgress = 0f;
+    float lastWrapped = 0f;
+
+    float secondsDiff => (targetTimeProgress - audioProgress) * source.clip.length;
 
     bool scratching = false;
-    float targetTimeProgress = 0f; // normalized 0-1
-    float timeProgress => source.time / source.clip.length; // normalized 0-1
-    float secondsDiff => (targetTimeProgress - timeProgress) * source.clip.length;
+    bool catchingUp = false;
+    float catchUpTimer = 0f;
+    float deltaAngle = 0f;
 
-    float angleDelta = 0f;
-
+    float inputAngle = 0;
+    float angleReference = 0;
 
     void Start()
     {
-        //slider = GetComponent<Slider>();
+        source.loop = true;
         source.Play();
     }
 
-    /*
-    void Update()
+    private void Update()
     {
         UpdateNeedle();
 
-        //close. Main problems are:
-        //that it's not looping correctly. When you do a full rotation it seems to lerp backwards through the whole song and it doesn't go to the end of the song when you go backwards from the start
-        //One of these equations is desyncing the sound from the record visual when you grab it
+        float wrapped = timeProgress;
+        float step = wrapped - lastWrapped;
+        if (step > 0.5f) step -= 1f;
+        else if (step < -0.5f) step += 1f;
+        audioProgress += step;
+        lastWrapped = wrapped;
+
         if (scratching)
         {
             inputAngle = GetAngle();
-            float angleDelta = inputAngle - angleReference;
+            deltaAngle = inputAngle - angleReference;
+            deltaAngle -= Mathf.Round(deltaAngle);
             angleReference = inputAngle;
+            targetTimeProgress += deltaAngle;
 
-            //lotta bullshit math here that's probably causing the desync. I know the normal rotation matches a pitch of 1 so im trying to reverse match the pitch to a certain rotation. the 32 was just brute forced until it felt right, probably an actually correct value to put there
-            source.pitch = Mathf.Lerp(source.pitch, angleDelta / Time.deltaTime * 32, 1 - Mathf.Pow(10, -Time.deltaTime));
-
-            //record.transform.Rotate(0, 0, -(angleDelta * 360));
-            targetTimeProgress += angleDelta;
-            var timeProgress = source.time / source.clip.length;
-            record.transform.SetLocalPositionAndRotation(record.transform.localPosition, Quaternion.Euler(0, 0, -timeProgress * 360f));
+            source.pitch = secondsDiff / followLag;
         }
         else
         {
-            source.pitch = 1;
-
-            //I know this is right
-            //record.transform.Rotate(0, 0, -(Time.deltaTime * (360f)) / source.clip.length);
-            targetTimeProgress += Time.deltaTime;
-
-            var timeProgress = source.time / source.clip.length;
-            record.transform.SetLocalPositionAndRotation(record.transform.localPosition, Quaternion.Euler(0, 0, -timeProgress * 360f));
+            CatchUp();
         }
-
-        logTimeline();
-    }
-    */
-
-    private bool wasBehind = true;
-
-    private void Update()
-    {
-        // Update targetTimeProgress...
-        if (scratching)
-        {
-            // either by rotating the disk manually,
-            inputAngle = GetAngle();
-            angleDelta = inputAngle - angleReference;
-            angleReference = inputAngle;
-            targetTimeProgress += angleDelta;
-        }
-        else if (!IsAudioBehind())
-            // or simply playing forward
-            targetTimeProgress = timeProgress;
-
-        // Update source.time & source.pitch...
-        if (scratching || IsAudioBehind())
-        {
-            // either by lerping to target,
-            LerpAudio(angleDelta, 0.5f);
-        }
-        if (wasBehind != IsAudioBehind())
-        {
-            // or snapping to be fully caught up
-            if (wasBehind && !scratching)
-            {
-                source.pitch = 1f;
-                source.time = targetTimeProgress * source.clip.length;
-            }
-            wasBehind = !wasBehind;
-        }
-
-
-        //Debug.Log(IsAudioBehind());
-        LogTimeline();
 
         UpdateRotation(true);
+    }
+
+    // After release
+    private void CatchUp()
+    {
+        if (!catchingUp)
+        {
+            // Caught up
+            targetTimeProgress = audioProgress;
+            source.pitch = 1f;
+            return;
+        }
+
+        catchUpTimer -= Time.deltaTime;
+        float gap = secondsDiff;
+
+        if (catchUpTimer <= 0f || Mathf.Abs(gap) <= 0.001f)
+        {
+            SetAudioProgress(targetTimeProgress); // land exactly
+            source.pitch = 1f;
+            catchingUp = false;
+            return;
+        }
+
+        float playable = maxPitch * catchUpTimer;
+        if (Mathf.Abs(gap) > playable)
+        {
+            SetAudioProgress(targetTimeProgress - Mathf.Sign(gap) * playable / source.clip.length);
+            gap = secondsDiff;
+        }
+
+        source.pitch = gap / catchUpTimer;
+    }
+
+    private void SetAudioProgress(float progress)
+    {
+        audioProgress = progress;
+        float w = Mathf.Repeat(progress, 1f);
+        source.time = w * source.clip.length;
+        lastWrapped = w;
     }
 
     private void UpdateRotation(bool debugMode = false)
@@ -109,39 +108,6 @@ public class Scratcher : MonoBehaviour
         float progress = debugMode ? timeProgress : targetTimeProgress;
         record.transform.SetLocalPositionAndRotation(record.transform.localPosition, Quaternion.Euler(0, 0, -progress * 360f));
     }
-
-    private void LerpAudio(float angleDelta, float maxDelaySeconds = 0f)
-    {
-        //source.pitch = Mathf.Lerp(source.pitch, angleDelta / Time.deltaTime * 32, 1 - Mathf.Pow(10, -Time.deltaTime));
-        float maxSpeed = secondsDiff / maxDelaySeconds;
-        source.pitch = maxSpeed;
-    }
-
-    private float thresholdSeconds = 1f;
-    private bool IsAudioBehind()
-    {
-        return Mathf.Abs(secondsDiff) > thresholdSeconds;
-    }
-
-
-    float inputAngle = 0;
-    float angleReference = 0;
-    private void ComputeAngleDelta()
-    {
-        inputAngle = GetAngle();
-        float d = inputAngle - angleReference;
-        angleReference = inputAngle;
-        angleDelta = d;
-    }
-
-    private void LogTimeline()
-    {
-        var timeProgress = source.time / source.clip.length;
-        var rotationProgess = 1 - (record.transform.localRotation.eulerAngles.z / 360f);
-        Debug.Log($"Scratcher: timeProgress={timeProgress}, targetTimeProgress={targetTimeProgress}, rotations: {rotationProgess}" +
-            $" Diff = {secondsDiff}, IsBehind={IsAudioBehind()}, WasBehind={wasBehind}");
-    }
-
     private void OnMouseDown()
     {
         scratching = true;
@@ -151,9 +117,11 @@ public class Scratcher : MonoBehaviour
     private void OnMouseUp()
     {
         scratching = false;
+        catchingUp = true;
+        catchUpTimer = catchUpSeconds;
     }
 
-    //returns the delta change in angle
+    //returns the angle under the cursor, normalized 0-1
     float GetAngle()
     {
         RaycastHit hit;
