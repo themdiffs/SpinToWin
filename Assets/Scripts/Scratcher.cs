621eec0394d95b199e225c7926e14073a6cd65e3
Joseph Isaacs <joseph.isaacs@mymail.champlain.edu>
2026-06-22 10:44:32 -0400

    Break pre-damping alg

--- Assets/Scripts/Scratcher.cs
+++ Assets/Scripts/Scratcher.cs
@@ -3,6 +3,8 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using System.Runtime.CompilerServices;
+using System;
 
 public class Scratcher : MonoBehaviour
 {
@@ -10,9 +12,12 @@ public class Scratcher : MonoBehaviour
     [SerializeField] GameObject record;
 
     bool scratching = false;
-    float inputAngle = 0;
-    float angleReference = 0;
-    float targetTimeProgress = 0f;
+    float targetTimeProgress = 0f; // normalized 0-1
+    float timeProgress => source.time / source.clip.length; // normalized 0-1
+    float secondsDiff => (targetTimeProgress - timeProgress) * source.clip.length;
+
+    float angleDelta = 0f;
+
 
     void Start()
     {
@@ -20,16 +25,17 @@ public class Scratcher : MonoBehaviour
         source.Play();
     }
 
+    /*
     void Update()
     {
-        updateNeedle();
+        UpdateNeedle();
 
         //close. Main problems are:
         //that it's not looping correctly. When you do a full rotation it seems to lerp backwards through the whole song and it doesn't go to the end of the song when you go backwards from the start
         //One of these equations is desyncing the sound from the record visual when you grab it
         if (scratching)
         {
-            inputAngle = GetDeltaAngle();
+            inputAngle = GetAngle();
             float angleDelta = inputAngle - angleReference;
             angleReference = inputAngle;
 
@@ -55,29 +61,100 @@ public class Scratcher : MonoBehaviour
 
         logTimeline();
     }
+    */
 
-    private void logTimeline()
+    private bool wasBehind = true;
+
+    private void Update()
+    {
+        // Update targetTimeProgress...
+        if (scratching)
+        {
+            // either by rotating the disk manually,
+            inputAngle = GetAngle();
+            angleDelta = inputAngle - angleReference;
+            angleReference = inputAngle;
+            targetTimeProgress += angleDelta;
+        }
+        else if (!IsAudioBehind())
+            // or simply playing forward
+            targetTimeProgress = timeProgress;
+
+        // Update source.time & source.pitch...
+        if (scratching || IsAudioBehind())
+        {
+            // either by lerping to target,
+            LerpAudio(angleDelta, 0.5f);
+        }
+        if (wasBehind != IsAudioBehind())
+        {
+            // or snapping to be fully caught up
+            if (wasBehind && !scratching)
+            {
+                source.pitch = 1f;
+                source.time = targetTimeProgress * source.clip.length;
+            }
+            wasBehind = !wasBehind;
+        }
+
+
+        //Debug.Log(IsAudioBehind());
+        LogTimeline();
+
+        UpdateRotation(true);
+    }
+
+    private void UpdateRotation(bool debugMode = false)
+    {
+        float progress = debugMode ? timeProgress : targetTimeProgress;
+        record.transform.SetLocalPositionAndRotation(record.transform.localPosition, Quaternion.Euler(0, 0, -progress * 360f));
+    }
+
+    private void LerpAudio(float angleDelta, float maxDelaySeconds = 0f)
+    {
+        //source.pitch = Mathf.Lerp(source.pitch, angleDelta / Time.deltaTime * 32, 1 - Mathf.Pow(10, -Time.deltaTime));
+        float maxSpeed = secondsDiff / maxDelaySeconds;
+        source.pitch = maxSpeed;
+    }
+
+    private float thresholdSeconds = 1f;
+    private bool IsAudioBehind()
+    {
+        return Mathf.Abs(secondsDiff) > thresholdSeconds;
+    }
+
+
+    float inputAngle = 0;
+    float angleReference = 0;
+    private void ComputeAngleDelta()
+    {
+        inputAngle = GetAngle();
+        float d = inputAngle - angleReference;
+        angleReference = inputAngle;
+        angleDelta = d;
+    }
+
+    private void LogTimeline()
     {
         var timeProgress = source.time / source.clip.length;
         var rotationProgess = 1 - (record.transform.localRotation.eulerAngles.z / 360f);
-        Debug.Log($"Scratcher: time progress={timeProgress}, rotations: {rotationProgess} " +
-            $"Diff = {timeProgress - rotationProgess}");
+        Debug.Log($"Scratcher: timeProgress={timeProgress}, targetTimeProgress={targetTimeProgress}, rotations: {rotationProgess}" +
+            $" Diff = {secondsDiff}, IsBehind={IsAudioBehind()}, WasBehind={wasBehind}");
     }
 
     private void OnMouseDown()
     {
         scratching = true;
-        angleReference = GetDeltaAngle();
+        angleReference = GetAngle();
     }
 
     private void OnMouseUp()
     {
         scratching = false;
-        source.time = targetTimeProgress;
     }
 
     //returns the delta change in angle
-    float GetDeltaAngle()
+    float GetAngle()
     {
         RaycastHit hit;
         float angle = 0f;
@@ -92,7 +169,7 @@ public class Scratcher : MonoBehaviour
         return angle / 360f;
     }
 
-    void updateNeedle()
+    void UpdateNeedle()
     {
         if (Keyboard.current.spaceKey.isPressed)
         {
